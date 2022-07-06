5bc9de9050a5f21b7341b20b38c5caf1da46a7ec
Ehsan <ehsan.taghavie@gmail.com>
2022-07-06 12:26:19 -0400

    api and tests are working

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ if "DYNO" in os.environ and os.path.isdir(".dvc"):
 app = FastAPI()
 
 """ 
-To extract the options run the following code:
+To extract all the input types/classes run the following code:
      for feat in cat_features:
      print(data[feat].unique())
  """
@@ -72,21 +72,22 @@ class UserClassInfo(BaseModel):
     ]
 
     class Config:
+        # This is an example with less than 50k response
         schema_extra = {
             "example": {
-                "age": 27,
-                "workclass": 'State-gov',
-                "fnlgt": 77516,
-                "education": 'Bachelors',
-                "education_num": 13,
-                "marital_status": "Never-married",
-                "occupation": "Tech-support",
-                "relationship": "Unmarried",
+                "age": 42,
+                "workclass": 'Self-emp-not-inc',
+                "fnlgt": 37618,
+                "education": 'Some-college',
+                "education_num": 10,
+                "marital_status": "Married-civ-spouse",
+                "occupation": "Farming-fishing",
+                "relationship": "Husband",
                 "race": "White",
-                "sex": "Female",
-                "capital_gain": 2000,
+                "sex": "Male",
+                "capital_gain": 0,
                 "capital_loss": 0,
-                "hours_per_week": 35,
+                "hours_per_week": 60,
                 "native_country": 'United-States'
             }
         }
@@ -101,13 +102,15 @@ lb = joblib.load("model/lb.pkl")
 # Define GET function
 @app.get("/")
 async def get_items():
-    return {"message": "Hello User! This is an app to predict if someone's income will exceed $50,000/year."}
+    return {"message": "Welcome User! This is an app to predict whether or not someone's income will exceed $50,"
+                       "000/year."}
 
 
-# Define POST function
+# Define POST function to predict the income category based on input of all the 15 features
 @app.post("/predict")
-async def predict(input: UserClassInfo):
-    # Define categorical features
+async def predict(data_input: UserClassInfo):
+    # Define categorical features.
+    # We need them to process the input and create correct feature similar to the time of training
     cat_features = [
         "workclass",
         "education",
@@ -119,33 +122,31 @@ async def predict(input: UserClassInfo):
         "native-country",
     ]
 
-    # Load model, encoder and lb
-    classifier = joblib.load("model/model.pkl")
-    encoder = joblib.load("model/encoder.pkl")
-    lb = joblib.load("model/lb.pkl")
+    # Load model, encoder and lb from model folder
+    rf_classifier = joblib.load("model/model.pkl")
+    rf_encoder = joblib.load("model/encoder.pkl")
+    rf_lb = joblib.load("model/lb.pkl")
 
-    # Compiile input data
-    input = np.array([[
-        0,
-        input.age,
-        input.workclass,
-        input.fnlgt,
-        input.education,
-        input.education_num,
-        input.marital_status,
-        input.occupation,
-        input.relationship,
-        input.race,
-        input.sex,
-        input.capital_gain,
-        input.capital_loss,
-        input.hours_per_week,
-        input.native_country
+    # Create input data with the right format
+    test_input_from_web = np.array([[
+        data_input.age,
+        data_input.workclass,
+        data_input.fnlgt,
+        data_input.education,
+        data_input.education_num,
+        data_input.marital_status,
+        data_input.occupation,
+        data_input.relationship,
+        data_input.race,
+        data_input.sex,
+        data_input.capital_gain,
+        data_input.capital_loss,
+        data_input.hours_per_week,
+        data_input.native_country
     ]])
 
-    # Convert to pandas dataframe
-    df = pd.DataFrame(data=input, columns=[
-        "",
+    # Convert input data from web to pandas dataframe
+    df_input_data = pd.DataFrame(data=test_input_from_web, columns=[
         "age",
         "workclass",
         "fnlgt",
@@ -162,18 +163,20 @@ async def predict(input: UserClassInfo):
         "native-country"
     ])
 
-    X, _, _, _ = process_data(
-        df,
+    x_data, _, _, _ = process_data(
+        df_input_data,
         categorical_features=cat_features,
-        encoder=encoder,
+        encoder=rf_encoder,
         lb=lb,
         training=False
     )
 
-    y_pred = inference(classifier, X)
-    y = lb.inverse_transform(y_pred)[0]
-    return {"prediction": y}
+    # Run the inference on the data sample
+    y_prediction = inference(rf_classifier, x_data)
+    # Convert the prediction to the label
+    y = rf_lb.inverse_transform(y_prediction)[0]
+    return {"Income category is: ": y}
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
