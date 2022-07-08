7149fb461b33218581407a82f12e2ee1363d7b29
Ehsan <ehsan.taghavie@gmail.com>
2022-07-08 14:36:09 -0400

    WIP:deployment

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from training.ml.data import process_data
 # Set up DVC in Heroku
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
+    os.system("dvc remote add -df s3-bucket s3://myherukobucket")
     if os.system("dvc pull") != 0:
         exit("dvc pull failed")
     os.system("rm -r .dvc .apt/usr/lib/dvc")
